b3eebc237ed5ee88981d5ad90d1adfdf0795f8f8
Selcan Atak <192803025@ogr.cbu.edu.tr>
2024-11-10 00:32:14 +0300

    license_plate_recognition
    yolov7 custom model

--- ai_projects/license_plate_recognition/main.py
+++ ai_projects/license_plate_recognition/main.py
@@ -12,7 +12,8 @@ MODEL = config['model_predict']['model_path']
 SECONDARY_SCRIPT_PATH = config['model_predict']['secondary_script_path']
 OD_RESULTS_PATH = config['model_predict']['od_result_path']
 RESULT_PATH = config['model_predict']['result_path']
-PYTHON_EXE_PATH = config['model_predict']['python_exe_path']
+# PYTHON_EXE_PATH = config['model_predict']['python_exe_path']
+PYTHON_EXE_PATH = r'C:\Users\selca\selcanatak\.artificial-intelligence\venv\Scripts\python.exe'
 IMAGES_PATH = config['model_predict']['images_path']
 
 os.makedirs(RESULT_PATH, exist_ok=True)
@@ -21,6 +22,10 @@ paths_to_check = [MODEL, SECONDARY_SCRIPT_PATH, OD_RESULTS_PATH, RESULT_PATH, PY
 for path in paths_to_check:
     if not os.path.exists(path):
         print(f"Dosya bulunamadı: {path}")
+
+    else:
+        print(f"Dosya bulundu: {path}")
+
 command = [PYTHON_EXE_PATH, SECONDARY_SCRIPT_PATH, MODEL, IMAGES_PATH, RESULT_PATH]
 result = subprocess.run(command, capture_output=True, text=True)
 
